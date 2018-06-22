3c9372be8fb2ca0eb3138779196318620f50f7a3
Kanshi <github@kanshi.fr>
2018-06-23 01:53:16 +0200

    [GoLang] Moving service to /tmp, as tmpfs is noexec

--- golang/bootstrap.sh
+++ golang/bootstrap.sh
@@ -13,19 +13,37 @@ if ! [ -d ${WATCH_FOLDER} ]; then
 	echo "ERROR :: You must mount your golang source folder under '${BASE_FOLDER}''"
 	exit 1
 fi
-service=$(basename ${SOFT_FOLDER})
+output_bin=/go/bin/$(basename ${SOFT_FOLDER})
+exec_bin=/tmp/$(basename ${SOFT_FOLDER})
 export GOPID=/tmp/gosoft.pid
 
 # compiler function
 function compile() {
 	# building service
 	echo "INFO :: Compiling service"
-	go install ${SOFT_FOLDER}
+	CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ${output_bin} ${SOFT_FOLDER}
 
-	# starting service
-	echo "INFO :: Starting service"
-	${service} &
-	echo $! > ${GOPID}
+	# restart service if compilation went well
+	if [ $? -eq 0 ]; then
+		# killing existing service if any
+		if [ -f ${GOPID} ]; then
+			# killing old process
+			echo -e "\n\n\nINFO :: Killing old process ${output_bin}"
+			pid=$(cat ${GOPID})
+			kill ${pid}
+
+			# Waiting for old process to stop
+			echo "INFO :: Waiting for old process to stop"
+			while [ $(ps aux | awk '{print $2}' | grep ${pid}) ]; do sleep 1; done
+			rm ${GOPID} $(which ${output_bin})
+		fi
+
+		# starting service
+		echo "INFO :: Starting service"
+		mv ${output_bin} ${exec_bin} # moving to /tmp in case /go/bin is tmpfs (noexec)
+		${exec_bin} &
+		echo $! > ${GOPID}
+	fi
 }
 
 # building project for the first time
@@ -36,16 +54,6 @@ echo "INFO :: Starting iwatch to automatically rebuild project inside ${WATCH_FO
 while true; do
 	# waiting for files to be modified
 	inotifywait -e create -e delete -e modify -e moved_to -r ${BASE_FOLDER}
-
-	# killing old process
-	echo -e "\n\n\nINFO :: Killing old process ${service}"
-	pid=$(cat ${GOPID})
-	kill ${pid}
-
-	# Waiting for old process to stop
-	echo "INFO :: Waiting for old process to stop"
-	while [ $(ps aux | awk '{print $2}' | grep ${pid}) ]; do sleep 1; done
-	rm ${GOPID} $(which ${service})
 
 	# rebuilding service
 	compile
